cf0a9a5cfca007af48606a43c5f40aff57e5ed75
Sayed Mubarish <mubarish007@gmail.com>
2025-07-18 17:35:32 +0530

    model instances -- bulkCreate and validation

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ const User = sequelize.define(
     username: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        len: [4, 16],
+      },
     },
     password: {
       type: DataTypes.STRING,
@@ -38,34 +41,31 @@ const User = sequelize.define(
 User.sync({ alter: true })
   .then(() => {
     console.log("Table synced according to model");
-    //Inserting data with build and save
-    /* 
-    const user = User.build({username : "Syed", password : "syedpass", age :25, eligible:true});
-    return user.save() 
-     */
-
-    // Insering data with create which is (build +save)
-
-    return User.create({
-        username : "Thangal",
-        password : "thangalpass",
-        age : 25,
-        eligible : true
-    })
+    return User.bulkCreate(
+      [
+        {
+          username: "Thangal",
+          password: "thangalpass",
+          age: 25,
+          eligible: true,
+        },
+        {
+          username: "Tom vercity",
+          password: "Tommy",
+          age: 32,
+          eligible: false,
+        },
+      ],
+      { validate: true }
+    );
   })
+
   .then((data) => {
-    console.log("Saved succesfully");
-    data.username = "John" //updating inserted data with save
-    data.age = 84
-    
-    return data.save()  //data.destroy()- deletes the data, data.reload() set returns the original data 
-    //Only update specific fields
-    // return data.save({fields:['username']})
+    console.log("users saved");
+    data.forEach((element) => {
+      console.log(element.toJSON());
+    });
   })
-  .then((data)=>{
-    console.log("user updated");
-    console.log(data.toJSON());
-  })
-  .catch(() => {
-    console.log("Some error occured");
+  .catch((error) => {
+    console.log("Some error occured",error);
   });
