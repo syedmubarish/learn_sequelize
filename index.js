5617053116f6fd99062d41186afcb4d29c9d57a9
Sayed Mubarish <mubarish007@gmail.com>
2025-07-18 15:32:14 +0530

    implement model basics

--- index.js
+++ index.js
@@ -1,14 +1,49 @@
-const Sequelize = require("sequelize")
+const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize('sequelizetut','root','iamgroot',
-    {
-        dialect:"mysql"
+const sequelize = new Sequelize("sequelizetut", "root", "iamgroot", {
+  dialect: "mysql",
+});
+
+
+
+//Creating model with define
+const User = sequelize.define(
+  "user",
+  {
+    user_id: {
+        type : Sequelize.DataTypes.INTEGER,
+        primaryKey : true,
+        autoIncrement : true
+    },
+    username: {
+      type: Sequelize.DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: Sequelize.DataTypes.STRING,
+    },
+    age: {
+      type: Sequelize.DataTypes.INTEGER,
+      defaultValue: 18,
+    },
+    eligible:{
+        type:Sequelize.DataTypes.BOOLEAN,
+        defaultValue : true
     }
-)
+  },
+  {
+    freezeTableName: true,
+    timestamps: false,
+  }
+);
 
-sequelize.authenticate().then(()=>{
-    console.log("connection succesfull");
-})
-.catch((error)=>{
-    console.log("connection failed:",error);
-})
+//Inserting table into Db using sync
+
+User.sync({force:true})
+  .then(() => {
+    console.log("Table synced according to model");
+  })
+  .catch(() => {
+    console.log("Some error occured");
+  });
+
