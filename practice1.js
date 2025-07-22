af2d834ac24792f54478d3d66b1664ec3ee43475
Sayed Mubarish <mubarish007@gmail.com>
2025-07-22 13:26:37 +0530

    test your knowledge

---
+++ practice1.js
@@ -0,0 +1,57 @@
+const Sequelize = require("sequelize");
+const { DataTypes, Op } = Sequelize;
+
+const sequelize = new Sequelize("sequelizetut", "root", "iamgroot", {
+  dialect: "mysql",
+});
+
+const Student = sequelize.define(
+  "student",
+  {
+    student_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4, 20],
+      },
+    },
+    favorite_class: {
+      type: DataTypes.STRING(25),
+      defaultValue: "Computer Science",
+    },
+    school_year: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    subscribed_to_bititude: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+  },
+  { freezeTableName: true, timestamps: false }
+);
+
+Student.sync({ alter: true })
+  .then(() => {
+    return Student.findAll({
+      attributes: [
+        "school_year",
+        [sequelize.fn("COUNT", sequelize.col("school_year")), "num_students"],
+      ],
+      group: ["school_year"],
+      order: [["num_students", "ASC"]],
+    });
+  })
+  .then((data) => {
+    data.forEach((element) => {
+      console.log(element.toJSON());
+    });
+  })
+  .catch((err) => {
+    console.log("Sync error", err);
+  });
