8caebe42c1306ae1514ee4751c93d36b721538a8
Sayed Mubarish <mubarish007@gmail.com>
2025-07-24 12:29:09 +0530

    post.setUser(user) of belongsTo

--- associations2.js
+++ associations2.js
@@ -40,12 +40,17 @@ let user, post;
 sequelize
   .sync({ alter: true })
   .then(() => {
-    return User.destroy({ where: { username: "Ironman" } });
+    return User.findOne({ where: { username: "Thor" } });
   })
   .then((data)=>{
-    console.log(data);
-    
+    user = data
+    return Post.findOne()
   })
+  .then((data)=>{
+    post = data
+    post.setUser(user)
+  })
+  
   .catch((err) => {
     console.log("Some error occured",err);
   });
